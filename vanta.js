bea52501e235d635af0ec81f5217be879fecbed2
Tushar chourasiya <chourasiyatushar53@gmail.com>
2025-06-29 21:03:11 +0530

    Swiper

--- vanta.js
+++ vanta.js
@@ -1,12 +1,12 @@
-VANTA.FOG({
-  el: "#body",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  highlightColor: 0xf2e9cd,
-  midtoneColor: 0x8a43cf,
-  lowlightColor: 0x806fd1,
-  baseColor: 0xf9f1f1
-})
+// VANTA.FOG({
+//   el: "#body",
+//   mouseControls: true,
+//   touchControls: true,
+//   gyroControls: false,
+//   minHeight: 200.00,
+//   minWidth: 200.00,
+//   highlightColor: 0xf2e9cd,
+//   midtoneColor: 0x8a43cf,
+//   lowlightColor: 0x806fd1,
+//   baseColor: 0xf9f1f1
+// })
